8bcb4fca0230caf38dee50490d09c1755d79e820
SK9486 <valli74appan@gmail.com>
2026-06-12 13:35:49 +0530

    Update progrma1.java

--- 12_06_2025/progrma1.java
+++ 12_06_2025/progrma1.java
@@ -1,1 +1,48 @@
-https://github.com/SK9486/rhp-2025-sk/blob/main/11_06_2026/program2.java
+import java.util.Scanner;
+
+public class TrappingRainwater {
+
+    public static int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] height = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+
+        System.out.println(trap(height));
+
+        sc.close();
+    }
+}
